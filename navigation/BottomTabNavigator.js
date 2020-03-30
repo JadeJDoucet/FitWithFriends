21be22a8e83ba33bd1f23c967a55983ea7a42630
jadejdoucet <jadejdoucet@gmail.com>
2020-03-30 12:12:31 -0500

    updated tab names

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -19,15 +19,15 @@ export default function BottomTabNavigator({ navigation, route }) {
         name="Home"
         component={HomeScreen}
         options={{
-          title: 'Get Started',
+          title: 'Home',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
         }}
       />
       <BottomTab.Screen
-        name="Links"
+        name="Profile"
         component={LinksScreen}
         options={{
-          title: 'Resources',
+          title: 'Profile',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
         }}
       />
@@ -40,8 +40,8 @@ function getHeaderTitle(route) {
 
   switch (routeName) {
     case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
+      return 'Home';
+    case 'Profile':
+      return 'Profile';
   }
 }
