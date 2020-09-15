53a7815804d88792eb971dca2d4b496536225575
jadejdoucet <jadejdoucet@gmail.com>
2020-09-15 16:54:03 -0500

    added dynamic background and state item based on clicked day of the week

--- components/WeeklyCalendar.js
+++ components/WeeklyCalendar.js
@@ -2,44 +2,66 @@ import * as React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-const names = ['Extreme Core', 'Extreme Chest', 'Get Fit Quick', 'Sweat It Out'];
-
 export default function WeeklyCalendar(props) {
     // This will use props to set the day clicked, but will use date function to set current day
     // Current day will have different color
+    const [calDay, setCalDay] = React.useState('mon');
+    
+    function getDay(day) {
+      if (day === calDay) {
+        return styles.selectedDay;
+      } else {
+        return styles.calendarDay;
+      }
+    }
+
     return (
-      <ScrollView style={styles.workoutStream1} horizontal>
-        <TouchableOpacity style={styles.workoutStreamItem}>
+      <ScrollView style={styles.calendarWeek} horizontal>
+        <TouchableOpacity style={getDay('sun')} onPress={() => setCalDay('sun')}>
+            <Text>Sun</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={getDay('mon')} onPress={() => setCalDay('mon')}>
             <Text>Mon</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.workoutStreamItem}>
+        <TouchableOpacity style={getDay('tue')} onPress={() => setCalDay('tue')}>
             <Text>Tues</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.workoutStreamItem}>
+        <TouchableOpacity style={getDay('wed')} onPress={() => setCalDay('wed')}>
             <Text>Wed</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.workoutStreamItem}>
+        <TouchableOpacity style={getDay('thur')} onPress={() => setCalDay('thur')}>
             <Text>Thurs</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.workoutStreamItem}>
+        <TouchableOpacity style={getDay('fri')} onPress={() => setCalDay('fri')}>
             <Text>Fri</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.calendarDay, getDay('sat')} onPress={() => setCalDay('sat')}>
+            <Text>Sat</Text>
         </TouchableOpacity>
     </ScrollView>
         );
 }
 
 const styles = StyleSheet.create({
-  workoutStreamItem: {
+  calendarDay: {
     // This will be a paper-like box with a workout
     height: 50,
-    width: 60,
+    width: 70,
     padding: 5,
-    margin: 5,
-    backgroundColor: 'grey',
+    margin: 1,
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  workoutStream1: {
+  calendarWeek: {
     // This will be a horizontal stream, side scrolling eventually
     flexDirection: 'row',
+  },
+  selectedDay: {
+    backgroundColor: 'lightgrey',
+    height: 50,
+    width: 70,
+    padding: 5,
+    margin: 1,
+    alignItems: 'center',
   }
 })
